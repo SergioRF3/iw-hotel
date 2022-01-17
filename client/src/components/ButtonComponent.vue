0027f63aca8f4a9769a1bdea8ee3992d75c38bab
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-17 19:33:59 +0100

    Añadido componente boton

---
+++ client/src/components/ButtonComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button"><a>{{nombre}}</a></div>
+</template>
+
+<script>
+export default {
+    name: 'ButtonComponent',
+    props: {
+        nombre: {
+            type: String,
+            default: ''
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.button {
+    display: flex;
+    flex-direction: row;
+}
+.button a{
+    display: block;
+    padding: 10px 15px;
+    background: black;
+    border-radius: 20px;
+    font-size: 14px;
+    color: #FFFFFF;
+}
+
+</style>
