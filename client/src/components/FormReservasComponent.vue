db7004a526caefd7c83ebd1d92ad8cfcbd4e87ce
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-18 19:15:14 +0100

    Creado el componente de reservas para administrador

---
+++ client/src/components/FormReservasComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form"><h2>Esta es la vista de reservas de admininstrador</h2><ButtonComponent /></div>
+</template>
+
+<script>
+import ButtonComponent from '@/components/ButtonComponents.vue'
+
+export default {
+    name: 'FormReservasComponent',
+    components: {
+        ButtonComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
