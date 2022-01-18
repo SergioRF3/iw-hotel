88d38e9bef9f138bf6caad09d435e1ff32cbbad9
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-18 15:28:08 +0100

    Añadida la tabla de todas las instalaciones que hay en la base de datos

--- client/src/components/FormInstalacionesComponent.vue
+++ client/src/components/FormInstalacionesComponent.vue
@@ -1,30 +1,59 @@
 <template>
-  <div class="form">
-    <div class="row">
-      <div class="col-6">
-        <div class="field">
-          <label class="titulo">Nombre</label>
-          <input type="text" id="nombre" placeholder="Instalación" />
+  <div class="instalaciones">
+    <div class="form">
+      <div class="row">
+        <div class="col-6">
+          <div class="field">
+            <label class="titulo">Nombre</label>
+            <input type="text" id="nombre" placeholder="Instalación" />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="field">
+            <label class="titulo">Imagen</label>
+            <input type="text" id="nombre" placeholder="imagen.jpg" />
+          </div>
         </div>
       </div>
-      <div class="col-6">
-        <div class="field">
-          <label class="titulo">Imagen</label>
-          <input type="text" id="nombre" placeholder="imagen.jpg" />
+      <div class="row">
+        <div class="col-12">
+          <div class="field">
+            <label class="titulo">Descripción</label>
+            <textarea
+              name="textarea"
+              id="textarea"
+              placeholder="Introduce aqui la descripción de la instalación"
+              cols="30"
+              rows="6"
+            ></textarea>
+          </div>
         </div>
+      </div>
+      <div class="botones">
+        <ButtonComponent nombre="Introducir" />
+        <ButtonComponent nombre="Volver" />
       </div>
     </div>
-    <div class="row">
-      <div class="col-12">
-        <div class="field">
-          <label class="titulo">Descripción</label>
-          <textarea name="textarea" id="textarea" placeholder="Introduce aqui la descripción de la instalación" cols="30" rows="6"></textarea>
-        </div>
-      </div>
-    </div>
-    <div class="botones">
-      <ButtonComponent nombre="Introducir" />
-      <ButtonComponent nombre="Volver" />
+    <div class="tabla">
+        <table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">Imagen</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <!-- <tr v-for="vu in valoresUsuarios" :key="vu.id">
+      <th>{{vu.id}}</th>
+      <td>{{vu.nombre}}</td>
+      <td><button class="btn btn-danger btn-xs" @click="verDetallesUsuario(vu.id)">Ver Detalles</button></td>
+      <td><button class="btn btn-primary btn-xs" @click="borrarUsuario(vu.id, $event)" >X</button></td>
+    </tr>  -->
+  </tbody>
+</table>
     </div>
   </div>
 </template>
@@ -40,6 +69,10 @@ export default {
 </script>
 
 <style>
+.tabla {
+    margin: 10vh auto;
+    width: 80%;
+}
 .form {
   width: 60%;
   background-color: #b5b2b2;
@@ -65,11 +98,11 @@ export default {
   border-radius: 10px;
 }
 
-.field textarea{
-    background-color: white;
-    border-width: 0px;
-    padding: 5px 0px 5px 10px;
-    border-radius: 10px;
+.field textarea {
+  background-color: white;
+  border-width: 0px;
+  padding: 5px 0px 5px 10px;
+  border-radius: 10px;
 }
 
 .cuenta {
