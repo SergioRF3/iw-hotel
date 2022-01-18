ce06d5adb4c9118e5be5e54ecffecf321a863fe8
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-18 19:43:08 +0100

    Añadido el template del formulario de servicios en admin

--- client/src/components/FormServiciosComponent.vue
+++ client/src/components/FormServiciosComponent.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="form"><h2>Esta es la vista de servicio de administrador</h2><ButtonComponent /></div>
+  <div class="instalaciones">
+    <div class="form">
+      <div class="row">
+        <div class="col-4">
+          <div class="field">
+            <label class="titulo">Nombre</label>
+            <input type="text" id="nombre" placeholder="Plancha ropa" />
+          </div>
+        </div>
+        <div class="col-8">
+          <div class="field">
+            <label class="titulo">Descripción</label>
+            <input type="text" id="descripcion" placeholder="El cliente deja su ropoa para que sea planchada" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <div class="field">
+            <label class="titulo">Tipo</label>
+            <input type="text" id="tipo" placeholder="PLR" />
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="field">
+            <label class="titulo">Precio</label>
+            <input type="text" id="precio" placeholder="10€" />
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="field">
+            <label class="titulo">Estado</label>
+            <input type="text" id="estado" placeholder="Disponible" />
+          </div>
+        </div>
+      </div>
+      <div class="botones">
+        <ButtonComponent nombre="Introducir" />
+        <ButtonComponent nombre="Volver" />
+      </div>
+    </div>
+    <div class="tabla">
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- Ejemplo para hacer la tabla de las servicios
+            <tr v-for="vu in valoresUsuarios" :key="vu.id">
+            <th>{{vu.id}}</th>
+            <td>{{vu.nombre}}</td>
+            <td><button class="btn btn-danger btn-xs" @click="verDetallesUsuario(vu.id)">Ver Detalles</button></td>
+            <td><button class="btn btn-primary btn-xs" @click="borrarUsuario(vu.id, $event)" >X</button></td>
+            </tr>  -->
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
