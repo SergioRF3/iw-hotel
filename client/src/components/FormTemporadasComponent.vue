cb8ad16bb9ec250a6a9bdef444134b08d5cc4ae2
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-18 19:49:10 +0100

    Creado el componente de formulario de temporadas en admin

---
+++ client/src/components/FormTemporadasComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form"><h2>Esta es la vista de Temporadas en el admin</h2><ButtonComponent/></div>
+</template>
+
+<script>
+import ButtonComponent from '@/components/ButtonComponent.vue'
+
+export default {
+    name: 'FormTemporadasComponent',
+    components: {
+        ButtonComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
