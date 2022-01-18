4edc25280505dc1c629e0b64e327544798b89756
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-18 19:34:10 +0100

    Creado el componente de formulario de servicios en el admin

---
+++ client/src/components/FormServiciosComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="form"><h2>Esta es la vista de servicio de administrador</h2><ButtonComponent /></div>
+</template>
+
+<script>
+import ButtonComponent from '@/components/ButtonComponent.vue'
+export default {
+    name: 'FormServiciosComponent',
+    components: {
+        ButtonComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
