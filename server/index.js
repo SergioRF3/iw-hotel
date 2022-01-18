1eb85b9c431ef02e930bce131921a745679dd944
JoaquinCerdan <jjcl4@alu.ua.es>
2022-01-18 02:05:20 -0800

    CRUD user y hall, añadidas dependencias

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 var express = require('express')
 var jwt = require('jwt-simple')
+var moment = require('moment')
+var multer = require('multer')
+var cors = require('cors')
+
+var app  = express()
+app.use(express.json())
+app.use(cors())
 
 var app  = express()
 app.use(express.json())
@@ -21,6 +28,56 @@ var secret = '79457453290676546685234867529018';
 app.listen(3000, function() {
     console.log("El servidor está en el puerto 3000")
 })
+
+//---------------------------------------------- Pagination ----------------------------------------------------
+
+async function pagination(table, currentPage, limit, order, search){
+    currentPage = parseInt(currentPage)
+    limit = parseInt(limit)
+    if(!currentPage || currentPage<0){
+        currentPage = 1
+    }
+    if(!limit || limit<0){
+        limit = 10
+    }
+    var offset = ((currentPage - 1) * limit)
+
+    var total = await knex(table).count()
+    .modify(function(queryBuilder) {
+        if (search != undefined) {
+           queryBuilder.whereRaw(`LOWER(${order}) LIKE ?`, [`%${search.toLowerCase()}%`])
+        }
+    })
+    var list = await knex(table)
+        .limit(limit)
+        .offset(offset)
+        .select()
+        .orderBy(order)
+        .modify(function(queryBuilder) {
+            if (search != undefined) {
+               queryBuilder.whereRaw(`LOWER(${order}) LIKE ?`, [`%${search.toLowerCase()}%`])
+            }
+         })
+
+    listCount = await list.length
+    total = await total[0].count
+
+    var totalPages = Math.ceil(total / limit)
+    var previousPage = 'http://localhost:3000/' + table + '?page=' + (currentPage - 1) + '&limit=' + limit
+    var nextPage = 'http://localhost:3000/' + table + '?page=' + (currentPage + 1) + '&limit=' + limit
+
+    return{
+        paging: {
+            total: total,
+            page: currentPage,
+            pages: totalPages,
+            limit: limit,
+            previous: previousPage,
+            next: nextPage,
+        },
+        data: list,
+    }
+}
 
 //---------------------------------------------- Autentification ----------------------------------------------------
 app.post('/login', async function(req, res){
@@ -74,4 +131,94 @@ function checkJWT(req, res, next) {
   }
 }
 
+//---------------------------------------------- User ---------------------------------------------------------
+//View user given an id
+app.get('/users/:id', async function(req,res) {
+    var user = await knex('user').select().where('id',req.params.id)
+    res.status(200).send(user)
+})
 
+//View list of all users
+app.get('/users', async function(req,res) {
+    var currentPage = req.query.page
+    var limit = req.query.limit
+    var search  =req.query.search
+    res.status(200).send(await pagination('user', currentPage, limit, 'email', search))
+})
+
+//Create user
+//Query returns the new autoincremented id
+app.post('/users', checkJWT, async function(req, res) {
+    var user = req.body
+    var id = await knex('user')
+        .insert({email: user.email,
+                name: user.name,
+                password: user.password})
+        .returning('id')
+    res.status(201).send('http://localhost:3000/users/' + id)
+})
+
+//Modify user given an existing one
+app.put('/users', checkJWT, async function(req,res) {
+    var user = req.body
+    await knex('user')
+        .update({email: user.email,
+                name: user.name,
+                password: user.password})
+        .where('id', user.id)
+    res.status(201).send('http://localhost:3000/users/' + user.id)
+})
+
+//Deletes user given an id
+app.delete('/users', checkJWT, async function(req,res) {
+    var id = req.body.id
+    await knex('user').del().where('id', id)
+    res.status(200).send('user ' + id + ' deleted succesfully')
+})
+
+//---------------------------------------------- Hall ---------------------------------------------------------
+//View hall given an id
+app.get('/halls/:id', async function(req,res) {
+    var hall = await knex('hall').select().where('id',req.params.id)
+    res.status(200).send(hall)
+})
+
+//View list of all halls
+app.get('/halls', async function(req,res) {
+    var currentPage = req.query.page
+    var limit = req.query.limit
+    var search  =req.query.search
+    res.status(200).send(await pagination('hall', currentPage, limit, 'number', search))
+})
+
+//Create hall
+//Query returns the new autoincremented id
+app.post('/hall', checkJWT, async function(req, res) {
+    var hall = req.body
+    var id = await knex('hall')
+        .insert({number: hall.number,
+                floor: hall.floor,
+                description: hall.description,
+                price: hall.price})
+        .returning('id')
+    res.status(201).send('http://localhost:3000/halls/' + id)
+})
+
+//Modify hall given an existing one
+app.put('/halls', checkJWT, async function(req,res) {
+    var hall = req.body
+    await knex('hall')
+        .update({number: hall.number,
+            floor: hall.floor,
+            description: hall.description,
+            price: hall.price})
+        .where('id', hall.id)
+    res.status(201).send('http://localhost:3000/halls/' + hall.id)
+})
+
+//Deletes hall given an id
+app.delete('/halls', checkJWT, async function(req,res) {
+    var id = req.body.id
+    await knex('hall').del().where('id', id)
+    res.status(200).send('hall ' + id + ' deleted succesfully')
+})
