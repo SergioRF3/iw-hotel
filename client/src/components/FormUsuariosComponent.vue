6d21755031fed42766b9e9100919d23b8122cc63
Andres Felipe Lopez Franco <aflf1@alu.ua.es>
2022-01-18 18:10:35 +0100

    Creado el componente de formulario de usuarios para administrador

---
+++ client/src/components/FormUsuariosComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="form"><h2>Pagina de formulario de usuarios admin</h2><ButtonConmponent /></div>
+</template>
+
+<script>
+import ButtonComponent from '@/components/ButtonComponent.vue'
+export default {
+    name: 'FormUsuariosComponent',
+    components: {
+        ButtonComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
